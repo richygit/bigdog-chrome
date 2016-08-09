3b9e43195bdf0a00649333d791ddd9090db91f4d
richard <richard@getup.org.au>
2016-08-09 13:43:51 +1000

    click on button to open comic in new page

--- myscript.js
+++ myscript.js
@@ -1,7 +1,11 @@
 $(function() {
   console.log("loaded extension!");
-  $(function() {
-    var url = $('.content__article-body.from-content-api.js-article__body figure a').attr('href')
+  var url = $('.content__article-body.from-content-api.js-article__body figure a').attr('href');
+  var iconUrl = chrome.extension.getURL("/link-new-tab.png");
+  var btn = $('<div id="bigdog" style="position:absolute;top:0;right:0;cursor:pointer"><img style="width:100px;opacity:0.5" src="' + iconUrl + '" /></div>');
+  var comic = $('.content__article-body');
+  comic.append(btn);
+  btn.click(function() {
     console.log("url", url);
     window.open(url, '_blank');
   });
